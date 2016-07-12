11586b14fcecc0dd3eaecb95a9c0bcddc1188e2f
Claudine <claudine.bananal@gmail.com>
2016-07-12 00:19:42 -0700

    initialize

---
+++ README.md
@@ -0,0 +1,14 @@
+#Claudine Bananal: Portfolio (Version 3.0)
+#####Theme Name: Dash
+This is a custom WordPress site based on the starter theme [Underscores] (http://www.underscores.me). It features selected works of mine in the fields of design and development as well as other creative pursuits. I also aim to make this an evolving project, wherein the production of which will help me constantly practice and improve my skills as well as try out and learn other techniques and tools of the trade that are new to me at this point in time.
+
+###Languages and Tools Used
+* HTML5 / CSS3
+* Flexbox
+* Media Queries
+* SASS
+* BEM Methodology
+* Javascript / jQuery
+* WordPress / PHP
+* MAMP / Gulp
+* Photoshop
---
+++ themes/dash/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Dash
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'dash' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'dash' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'dash' ), 'dash', '<a href="http://www.claudinebananal.com" rel="designer">Claudine Bananal</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
