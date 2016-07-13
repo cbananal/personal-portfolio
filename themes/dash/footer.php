e3ea8a4c317683fc3fc6dfbb1c5db5d7e0bfbb81
Claudine <claudine.bananal@gmail.com>
2016-07-12 19:13:04 -0700

    structure and style footer

--- themes/dash/footer.php
+++ themes/dash/footer.php
@@ -13,7 +13,14 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
+	<footer id="colophon" class="site-footer container" role="contentinfo">
+		<div class="social">
+			<a href="http://www.linkedin.com/in/cbananal" target="_blank"><i class="fa fa-linkedin-square"></i></a>
+			<a href="http://www.github.com/cbananal" target="_blank"><i class="fa fa-github"></i></a>
+			<a href="http://www.codepen.io/cbananal" target="_blank"><i class="fa fa-codepen"></i></a>
+			<a href="http://www.twitter.com/unfinished_art" target="_blank"><i class="fa fa-twitter"></i></a>
+		</div>
+		<p>Copyright 2016 / Claudine Bananal<br>Created, as always, with love.</p>
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'dash' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'dash' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
