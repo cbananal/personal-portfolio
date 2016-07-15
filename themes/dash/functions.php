0a7ff940c881ba71a92ffb181516dc59afbbc42e
Claudine <claudine.bananal@gmail.com>
2016-07-15 12:26:05 -0700

    point js pathing to build folder

--- themes/dash/functions.php
+++ themes/dash/functions.php
@@ -113,11 +113,11 @@ function dash_scripts() {
 
 	wp_enqueue_style( 'dash-animate', get_stylesheet_directory_uri() . '/build/css/animate.css');
 
-	wp_enqueue_script( 'dash-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'dash-navigation', get_template_directory_uri() . 'build/js/navigation.min.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'dash-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'dash-skip-link-focus-fix', get_template_directory_uri() . 'build/js/skip-link-focus-fix.min.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'dash-scripts', get_template_directory_uri() . '/js/main.js', array(), false, true );
+	wp_enqueue_script( 'dash-scripts', get_template_directory_uri() . 'build/js/main.min.js', array(), false, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
