78dedc0ad6976c88437ac2bd525f45b7ff0ef054
Claudine <claudine.bananal@gmail.com>
2016-07-21 17:32:17 -0700

    reposition title of single posts outside flexed content

--- themes/dash/single-portfolio.php
+++ themes/dash/single-portfolio.php
@@ -9,28 +9,62 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="content-area container">
 		<main id="main" class="site-main" role="main">
+
+		<header class="entry-header">
+			<?php
+				if ( is_single() ) {
+					the_title( '<h1 class="entry-title">', '</h1>' );
+				} else {
+					the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+				}
+
+			if ( 'post' === get_post_type() ) : ?>
+			<div class="entry-meta">
+				<?php dash_posted_on(); ?>
+			</div><!-- .entry-meta -->
+			<?php
+			endif; ?>
+		</header><!-- .entry-header -->
 
 		<div class="project-content-container">
 			<div class="project-images">
-				<?php
-				while ( have_posts() ) : the_post();
+				<?php while ( have_posts() ) : the_post(); ?>
 
-					get_template_part( 'template-parts/content', get_post_format() );
+					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-					the_post_navigation();
+						<div class="entry-content">
+							<?php
+								the_content( sprintf(
+									/* translators: %s: Name of current post. */
+									wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'dash' ), array( 'span' => array( 'class' => array() ) ) ),
+									the_title( '<span class="screen-reader-text">"', '"</span>', false )
+								) );
 
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
+								wp_link_pages( array(
+									'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'dash' ),
+									'after'  => '</div>',
+								) );
+							?>
+						</div><!-- .entry-content -->
+
+						<footer class="entry-footer">
+							<?php dash_entry_footer(); ?>
+						</footer><!-- .entry-footer -->
+					</article><!-- #post-## -->
+
+					<?php the_post_navigation(); ?>
+
+					<!-- // If comments are open or we have at least one comment, load up the comment template. -->
+					<?php if ( comments_open() || get_comments_number() ) :
 						comments_template();
-					endif;
+					endif; ?>
 
-				endwhile; // End of the loop.
-				?>
+				<?php endwhile; ?>
 			</div>
 			<!--project-images-->
-			
+
 			<div class="project-details">
 				<?php echo CFS()->get ( 'more_information' )?>
 				<?php echo CFS()->get ( 'demo_link' )?>
